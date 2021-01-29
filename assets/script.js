717b95f1ad7e03013c56bd4e67e76b6779d3fa03
albenchris <albenchris00@gmail.com>
2021-01-29 15:25:08 -0600

    random drink displays with image, list of ingredients, and instructions

--- assets/script.js
+++ assets/script.js
@@ -4,14 +4,72 @@ var randomModalEl = document.getElementById("random-modal");
 var mainContainerEl = document.getElementById("main-content-container");
 
 function getRandomDrink() {
+    resetState();
+    mainContainerEl.innerHTML = "";
+
     var randomURL = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
 
     fetch(randomURL).then(function(response) {
-        return response.json();
-    }).then(function(response) {
-        mainContainerEl.innerHTML = "";
-        console.log(response);
+        return response.json().then(function(data) {
+            // console.log(data);
+            displayRandomCocktail(data);
+        })
+    }).catch(function(err) {
+        console.log("Error: ", err);
     });
+};
+
+function displayRandomCocktail(cocktail) {
+    // image start
+    var imgContainerEl = document.createElement("div");
+    imgContainerEl.setAttribute("id", "image-container");
+    imgContainerEl.classList.add("pur-u-1-4");
+    var image = document.createElement("img");
+    image.setAttribute("src", cocktail.drinks[0].strDrinkThumb);
+    imgContainerEl.appendChild(image);
+    // image end
+
+    // ingredients start
+    var ingredientsEl = document.createElement("div");
+    ingredientsEl.setAttribute("id", "ingredients-container");
+    ingredientsEl.classList.add("pur-u-1-4");
+    var drinkName = document.createElement("h3");
+    drinkName.classList.add("drink-name");
+    drinkName.innerHTML = cocktail.drinks[0].strDrink
+    var ingredientsList = document.createElement("ul");
+    ingredientsList.classList.add("ingredients-list");
+    for (var i=1; i<=15; i++) {
+        if (cocktail.drinks[0][`strIngredient${i}`] == null) {
+            break;
+        }
+        var ingredient = document.createElement("li");
+        ingredient.innerHTML = cocktail.drinks[0][`strIngredient${i}`];
+        if(cocktail.drinks[0][`strMeasure${i}`] == null) {
+            break;
+        }
+        var measurement = document.createElement("span")
+        measurement.innerHTML = " - " + cocktail.drinks[0][`strMeasure${i}`];
+        ingredient.appendChild(measurement);
+        ingredientsList.appendChild(ingredient);
+    }
+    ingredientsEl.appendChild(drinkName);
+    ingredientsEl.appendChild(ingredientsList);
+    // ingredients end
+
+    // instructions start
+    var instructionsEl = document.createElement("div");
+    instructionsEl.setAttribute("id", "instructions-container");
+    instructionsEl.classList.add("pur-u-1-4");
+    instructionsEl.innerHTML = 
+        "<h3 class='instructions'>Instructions</h3>" +
+        "<p>" +
+        cocktail.drinks[0][`strInstructions`] +
+        "</p>";
+    // instructions end
+
+    mainContainerEl.appendChild(imgContainerEl);
+    mainContainerEl.appendChild(ingredientsEl);
+    mainContainerEl.appendChild(instructionsEl);
 };
 
 function resetState() {
